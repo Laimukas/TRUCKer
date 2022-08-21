6afe77d4e83a7c3b641e004aa173f55c41bb61de
Laimukas <l.kestauskas@gmail.com>
2022-08-21 20:32:49 +0300

    Update

--- src/main/java/com/example/trucker/DriversPage4.java
+++ src/main/java/com/example/trucker/DriversPage4.java
@@ -8,9 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -65,6 +69,50 @@ public class DriversPage4 implements Initializable {
     private TextField tfPosition;
     @FXML
     private TextArea taNote;
+    @FXML
+    public Canvas canvasParasas;
+    private GraphicsContext gc;
+
+    @FXML
+    public void canvasParasasMouseDragged(MouseEvent mouseEvent) {
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+        gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+        gc.stroke();
+    }
+    @FXML
+    public void canvasParasasMousePressed(MouseEvent mouseEvent) {
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+        gc.beginPath();
+        gc.moveTo(mouseEvent.getX(), mouseEvent.getY());
+        gc.stroke();
+    }
+    @FXML
+    public void canvasParasasMouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    private void initDraw(GraphicsContext gc){
+        double canvasWidth = gc.getCanvas().getWidth();
+        double canvasHeight = gc.getCanvas().getHeight();
+
+        gc.setFill(Color.LIGHTGRAY);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+
+        gc.fill();
+        gc.strokeRect(
+                0,              //x of the upper left corner
+                0,              //y of the upper left corner
+                canvasWidth,    //width of the rectangle
+                canvasHeight);  //height of the rectangle
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+
+    }
 
     public void initData4(String id) throws TRUCKerListError {
 
@@ -164,6 +212,9 @@ public class DriversPage4 implements Initializable {
     }
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        gc = canvasParasas.getGraphicsContext2D();
+        initDraw(gc);
 
         finishedOrders = new FinishedOrderList();
         lineUps = new GeneratorList();
